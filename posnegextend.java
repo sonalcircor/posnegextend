7b1ef16e09b54eb04121f41c40a8be434d83f765
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-29 18:33:00 +0100

    posative negative extend

---
+++ posnegextend.java
@@ -0,0 +1,17 @@
+package ifelseHWposnegextend;
+
+public class posnegextend {
+    public static void main(String[] args) {
+        int num = -5;
+        if (num > 0){
+            System.out.println("num is positive");
+        }
+        else if(num < 0){
+            System.out.println("num is negative");
+        }
+        else{
+            System.out.println("num is zero");
+        }
+
+    }
+}
